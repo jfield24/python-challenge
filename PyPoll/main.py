b4b7434fc3d2b5118a724fdce97bc405183e7d41
Justin Field <justin_w_field@hotmail.com>
2020-09-08 04:23:44 +1000

    completed script and exported txt file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,11 +9,10 @@ os.chdir(os.path.dirname(__file__))
 csvpath = os.path.join("Resources", "election_data.csv")
 
 # Creates the lists we can append the necessary info to
-
 votes = []
 candidatenames = []
 
-
+# Creates votes per candidate list - need 4 '0' initial values because there are 4 candidates
 votespercandidate = [0, 0, 0, 0]
 
 # Reads the CSV file
@@ -27,10 +26,13 @@ with open(csvpath, 'r') as csv_file:
     for row in csv_reader:
         votes.append(row[0])
 
+        # Computes total votes
         totalvotes = len(votes)
 
+        # Sets where we are looking for the names of candidates in the CSV 
         candidatename = row[2]
 
+        # If and elif statements to find the unique candidate names, and add to their vote counts
         if candidatename not in candidatenames:
             candidatenames.append(candidatename)
 
@@ -46,8 +48,10 @@ with open(csvpath, 'r') as csv_file:
         elif candidatename == candidatenames[3]:
             votespercandidate[3] += 1
 
+# Create empty list for percentage of votes for each candidate
 percent = [0, 0, 0, 0]
 
+# Computing percentages of total votes for each candidate and formatting as percentage
 percent[0] = "{:.3%}".format(votespercandidate[0]/totalvotes)
 
 percent[1] = "{:.3%}".format(votespercandidate[1]/totalvotes)
@@ -56,17 +60,20 @@ percent[2] = "{:.3%}".format(votespercandidate[2]/totalvotes)
 
 percent[3] = "{:.3%}".format(votespercandidate[3]/totalvotes)
 
-
+# Computes the most votes for a candidate and matches it to their name
 mostvotes = max(votespercandidate)
 winner = candidatenames[(votespercandidate.index(mostvotes))]
 
+# Formats total votes per candidate by adding thousands separator
 votetotal = "{:,}".format(totalvotes)
 
+# Formats votes per candidate by adding thousands separator
 candvotes0 = "{:,}".format(votespercandidate[0])
 candvotes1 = "{:,}".format(votespercandidate[1])
 candvotes2 = "{:,}".format(votespercandidate[2])
 candvotes3 = "{:,}".format(votespercandidate[3])
 
+# Prints my analysis to the terminal
 print("Election Results")
 print("--------------------")
 print(f'Total Votes: {votetotal}')
@@ -83,7 +90,7 @@ print("--------------------")
 resultsfile = os.path.join("Analysis", "results.txt")
 
 with open (resultsfile, "w") as textfile:
-    textfile.write("Election Restults\n")
+    textfile.write("Election Results\n")
     textfile.write("--------------------\n")
     textfile.write(f'Total Votes: {votetotal}\n')
     textfile.write("--------------------\n")
@@ -91,4 +98,6 @@ with open (resultsfile, "w") as textfile:
     textfile.write(f'{candidatenames[1]}: {percent[1]} ({candvotes1})\n')
     textfile.write(f'{candidatenames[2]}: {percent[2]} ({candvotes2})\n')
     textfile.write(f'{candidatenames[3]}: {percent[3]} ({candvotes3})\n')
-    
+    textfile.write("--------------------\n")
+    textfile.write(f'Winner: {winner}\n')
+    textfile.write("--------------------")
