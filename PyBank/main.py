aacf83a0d07e8f95e8cc8f8132d0dbed6588c173
Justin Field <justin_w_field@hotmail.com>
2020-09-07 14:45:30 +1000

    Computed total months and profit and loss

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,22 +1,38 @@
+# Imports the modules we need
 import os
 import csv
 
+# Locates the CSV file used for analysis
 csvpath = os.path.join(".", "Resources", "budget_data.csv")
 
-totalmonths = []
-totalprofitloss = []
+# Creates the lists we can append the necessary info to
+months = []
+profitloss = []
 
-monthcount = 0
+# Holding variable at the start 
+totalprofitloss = 0
 
+# Reads the CSV file
 with open(csvpath, 'r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
 
+    # Skips the header
     csv_header = next(csv_file)
-    print(f"Header:{csv_header}")
 
+    # Loop through the data and append to the lists
     for row in csv_reader:
-        totalmonths.append(row[0])
-        totalprofitloss.append(row[1])
+        months.append(row[0])
+        profitloss.append(row[1])
+
+        # Computes the total number of months in the dataset
+        monthcount = len(months)
+        
+        # Adds to the initially empty variable totalprofitloss, and sums the total profit and loss
+        totalprofitloss += int(row[1])
+
+        
+
+
 
 
 
