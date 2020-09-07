e83c9d46e4533804bff8a39147301405e75c3501
Justin Field <justin_w_field@hotmail.com>
2020-09-07 18:02:21 +1000

    updated script

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,12 +10,8 @@ months = []
 profitloss = []
 monthlychanges = []
 
-# Sets the initial varaibles
-totalprofitloss = 0
-monthcount = 0
-openingprofitloss = 0
-closingprofitloss = 0
-monthlychange = 0
+# Sets an initial i value for use when looping
+i = 0
 
 # Reads the CSV file
 with open(csvpath, 'r') as csv_file:
@@ -35,45 +31,57 @@ with open(csvpath, 'r') as csv_file:
         # Adds to the initially empty variable totalprofitloss, and sums the total profit and loss
         totalprofitloss = sum(profitloss)
 
-        monthcount +=1
+        # Can format total profit/loss as a currency, adds a thousands separator 
+        totalprofitloss = "${:,}".format(totalprofitloss)
+        
+        # We are going to add 1 to i for each iteration
+        i += 1
         
         closingprofitloss = int(row[1])
 
-        if monthcount == 1:
+        # If it is the first row being iterated then set openingprofitloss as closingprofitandloss
+        if i == 1:
 
             openingprofitloss = closingprofitloss
 
+        # If that isn't the case, then..
         else: 
+            # Compute monthly change in profit/loss
             monthlychange = closingprofitloss - openingprofitloss
 
+            # Append this value to the list of monthly change
             monthlychanges.append(monthlychange)
 
+            # Makes the closingprofitloss the openingprofitloss for the next for loop
             openingprofitloss = closingprofitloss
 
-    averagemonthlychange = sum(monthlychanges)/(monthcount - 1)
+    # Calculated the average monthly change
+    averagemonthlychange = sum(monthlychanges)/(i - 1)
 
-    averagemonthlychange = round((averagemonthlychange),2)
+    # Format average monthly change as currency and add separator for thousands and cents
+    averagemonthlychange = "${:,.2f}".format(averagemonthlychange)
 
-    print(averagemonthlychange)
+    # Find the greatest increase in profits/losses over the entire period and respective month
+    greatestincrease = max(monthlychanges)
+    bestmonth = months[(monthlychanges.index(greatestincrease))+1]
 
+    # Convert greatest increase to currency
+    greatestincrease = "${:,}".format(greatestincrease)
 
+    # Find the greatest decrease in profits/losses over the entire period and respective month
+    greatestdecrease = min(monthlychanges)
+    worstmonth = months[(monthlychanges.index(greatestdecrease))+1]
 
+    # Convert greatest decrease to currency
+    greatestdecrease = "${:,}".format(greatestdecrease)
 
-            
+   
+# Prints my analysis to the terminal
 
-
-
-            
-            
-
-
-        
-
-
-
-
-
-
-
-
-
+print("Financial Analysis")
+print("--------------------")
+print(f'Total Months: {totalmonths}')
+print(f'Total: {totalprofitloss}')
+print(f'Average Change: {averagemonthlychange}')
+print(f'Greatest Increase in Profits: {bestmonth} ({greatestincrease})')
+print(f'Greatest Decrease in Profits: {worstmonth} ({greatestdecrease})')
